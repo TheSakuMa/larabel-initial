a8e3f1bf43277550ad1e3d8adc885a736f8c38f4
M.Sakurai <thesakumasan@gmail.com>
2021-05-01 11:51:36 +0900

    updated dependencies

--- app/Book.php
+++ app/Book.php
@@ -1,14 +1,11 @@
 <?php
-/**
- * Eloquent ORM はLaravelでデータ操作をするための実装である
- * このBookモデルはbooksテーブルにマッピングされており、テーブルの登録や取得更新などので操作を行っている
- * Bookモデルとした場合、特に指定しなければ、命名規則によりbooksテーブルとマッピングされる。
- */
+
 namespace App;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    use HasFactory;
 }
